ab2896002913ffe5f0f0d3b02e7a07cad12a54d1
jyoung217 <jonyoungg@gmail.com>
2019-01-30 13:42:08 -0600

    remove last import

--- ormQueries/products/tests.py
+++ ormQueries/products/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from .models import Product
 from .product_crud import ProductCrud
-import redgreenunittest as unittest
 
 def get_query_ids(queryset):
   return list(queryset.values_list('id', flat=True))
